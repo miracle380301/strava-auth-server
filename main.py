37241a93a6fe1f6eafd24648a9d184c3948cd1cc
miracle380301 <miracle380301@gmail.com>
2025-06-21 15:19:29 +0900

    modify sources

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ REPLIT_BACKEND_URL = os.getenv("REPLIT_BACKEND_URL")
 @app.get("/login")
 def login():
     print("@@@@ [Render] login @@@@")
+    print(os.environ)
     redirect_url = f"https://www.strava.com/oauth/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&approval_prompt=auto&scope=activity:read"
     print(f"@@@@ [Render] : {redirect_url}")
     return RedirectResponse(
